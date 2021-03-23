70b18810bace1c465e94c54e60b85315f5bce484
Katie And <56773311+Lilith-In-Starlight@users.noreply.github.com>
2021-03-23 13:26:00 -0500

    Create test function that reads messages

---
+++ sim.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+  "fmt"
+  "os"
+	"os/signal"
+	"syscall"
+
+  "github.com/bwmarrin/discordgo"
+)
+
+func main(){
+  fmt.Println("Initiated program")
+  discord, err := discordgo.New("Bot " + "ODIzOTgyNDg3MjcyMjkyMzcy.YFovfQ.JTEBZS8UlwXHeY9gDEiJJmgm3Ks")
+  if err != nil {
+    fmt.Println("Error creating Discord session: ", err)
+    return
+  }
+  discord.AddHandler(messageCreate)
+  discord.Identify.Intents = discordgo.IntentsGuildMessages
+  err = discord.Open()
+  if err != nil {
+    fmt.Println("Error opening Discord session: ", err)
+  }
+
+  // Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	discord.Close()
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	// Ignore all messages created by the bot itself
+	// This isn't required in this specific example but it's a good practice.
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+  if m.ChannelID == "823421429601140756" {
+    fmt.Println("Test")
+  }
+}
